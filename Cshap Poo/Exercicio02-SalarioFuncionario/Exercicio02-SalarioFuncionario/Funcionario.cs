78f6090a6a3a28f362ee458adf8fcf843ed61103
g-aleprojetos <g.aleprojetos@gmail.com>
2021-04-19 20:34:22 -0300

    exercicio 02 finalizado - Aumento de salário

---
+++ Cshap Poo/Exercicio02-SalarioFuncionario/Exercicio02-SalarioFuncionario/Funcionario.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+
+namespace Exercicio02_SalarioFuncionario
+{
+    class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+        public double NovoSalario;
+       
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentoSalario(double porcentagemAumento)
+        {
+
+            NovoSalario = SalarioBruto * ( 1 + (porcentagemAumento / 100)) - Imposto;
+        }
+
+        public override string ToString()
+        {
+            return "Dados atualizados: "
+                + Nome
+                + ", $ "
+                + NovoSalario.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Cshap Poo/Exercicio02-SalarioFuncionario/Exercicio02-SalarioFuncionario/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio02_SalarioFuncionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            double porcentagemAumento;
+
+            Funcionario funcionario1 = new Funcionario();
+
+            Console.WriteLine();
+            Console.Write("Nome:");
+            funcionario1.Nome = Console.ReadLine();
+
+            Console.Write("Salário Bruto:  ");
+            funcionario1.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Imposto: ");
+            funcionario1.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Funcionario: " + funcionario1.Nome + " $ " + funcionario1.SalarioLiquido());
+
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            porcentagemAumento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            funcionario1.AumentoSalario(porcentagemAumento);
+
+            Console.WriteLine(funcionario1);
+            Console.WriteLine();
+
+        }
+    }
+}
