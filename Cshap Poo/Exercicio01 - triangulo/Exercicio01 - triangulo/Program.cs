a3a3044dcd856e7b3338a0fabc807d154aac5391
g-aleprojetos <g.aleprojetos@gmail.com>
2021-04-19 19:46:26 -0300

    exercicio 01 retangulo finalizado

---
+++ Cshap Poo/Exercicio01 - triangulo/Exercicio01 - triangulo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio01___triangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double largura, altura;
+            Retangulo retangulo = new Retangulo();
+
+            Console.WriteLine("Entre com a largura e a altura do retângulo");
+            Console.Write("Largura: ");
+            largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Altura: ");
+            altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            retangulo.AreaRetangulo(largura, altura);
+            retangulo.PerimetroRetangulo(largura, altura);
+            retangulo.DiagonalRetangulo(largura, altura);
+
+            Console.WriteLine();
+            Console.WriteLine(retangulo);
+        }
+    }
+}
---
+++ Cshap Poo/Exercicio01 - triangulo/Exercicio01 - triangulo/Retangulo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Exercicio01___triangulo
+{
+    class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+        public double Area;
+        public double Perimetro;
+        public double Diagonal;
+
+        public void AreaRetangulo(double largura, double altura)
+        {
+            Area = largura * altura;
+        }
+
+        public void PerimetroRetangulo(double largura, double altura)
+        {
+            Perimetro = (largura * 2) + (altura * 2);
+        }
+
+        public void DiagonalRetangulo(double largura, double altura)
+        {
+            Diagonal = Math.Sqrt(Math.Pow(largura,2) + Math.Pow(altura, 2));
+        }
+
+        public override string ToString()
+        {
+            return "AREA = " 
+                + Area.ToString("F2", CultureInfo.InvariantCulture)
+                + "\nPerimetro = "
+                + Perimetro.ToString("F2", CultureInfo.InvariantCulture)
+                + "\nDiagonal = "
+                + Diagonal.ToString("F2", CultureInfo.InvariantCulture)
+                + "\n";
+        }
+    }
+}
