6608a004e131075e4265a4e01ae147893827ab59
g-aleprojetos <g.aleprojetos@gmail.com>
2021-04-22 11:21:01 -0300

    metodo

---
+++ Cshap Poo/ClassesAtributosMetodosStatic/ExerciciodeFixacaoMeEstatico-ConversorDeDolar/ConversorDeMoeda.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciciodeFixacaoMeEstatico_ConversorDeDolar
+{
+    class ConversorDeMoeda
+    {
+        public static double Iof = 0.06;
+
+
+        public static double Conversor(double valorDolar, double quantDolar)
+        {
+            return (quantDolar * valorDolar) * (1 + Iof);
+        }
+
+
+    }
+}
---
+++ Cshap Poo/ClassesAtributosMetodosStatic/ExerciciodeFixacaoMeEstatico-ConversorDeDolar/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExerciciodeFixacaoMeEstatico_ConversorDeDolar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual é a cotação do dolar?");
+            double valorDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Quantos dólares você vai comprar?");
+            double quantDolares = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Valor a ser pago em reais = " + ConversorDeMoeda.Conversor(valorDolar, quantDolares).ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
