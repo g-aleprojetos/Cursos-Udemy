175e3ded67c8258bb134e61997cd63371cdcc3ba
g-aleprojetos <g.aleprojetos@gmail.com>
2021-06-25 18:30:20 -0300

    list - part 1

---
+++ Cshap Poo/Comportamento de memoria arrays listas/Nullable/List/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+
+            List<string> list1 = new List<string> { "Maria", "João" };
+
+
+        }
+    }
+}
