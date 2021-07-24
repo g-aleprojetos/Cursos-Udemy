82197b3a26d8ef4be9a3f1c34579dc580d385238
g-aleprojetos <g.aleprojetos@gmail.com>
2021-07-24 16:12:13 -0300

    Seção 7: finalizado

---
+++ Cshap Poo/Secao7TopicosespeciaisEmCSahrp/OperacaoComTimeSpan/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace OperacaoComTimeSpan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan t1 = TimeSpan.MaxValue;
+            TimeSpan t2 = TimeSpan.MinValue;
+            TimeSpan t3 = TimeSpan.Zero;
+
+            Console.WriteLine(t1);
+            Console.WriteLine(t2);
+            Console.WriteLine(t3);
+            Console.WriteLine();
+
+            TimeSpan t4 = new TimeSpan(2, 3, 5, 7, 11);
+            
+            Console.WriteLine(t4);
+            
+            Console.WriteLine("Days: " + t4.Days);
+            Console.WriteLine("Hours: " + t4.Hours);
+            Console.WriteLine("Minutes: " + t4.Minutes);
+            Console.WriteLine("Milliseconds: " + t4.Milliseconds);
+            Console.WriteLine("Seconds: " + t4.Seconds);
+            Console.WriteLine("Ticks: " + t4.Ticks);
+            Console.WriteLine();
+
+            Console.WriteLine("TotalDays: " + t4.TotalDays);
+            Console.WriteLine("TotalHours: " + t4.TotalHours);
+            Console.WriteLine("TotalMinutes: " + t4.TotalMinutes);
+            Console.WriteLine("TotalSeconds: " + t4.TotalSeconds);
+            Console.WriteLine("TotalMilliseconds: " + t4.TotalMilliseconds);
+            Console.WriteLine();
+
+            TimeSpan t5 = new TimeSpan(1, 30, 10);
+            TimeSpan t6 = new TimeSpan(0, 10, 5);
+
+            TimeSpan sum = t5.Add(t6);
+            TimeSpan dif = t5.Subtract(t6);
+            TimeSpan mult = t5.Multiply(2.0);
+            TimeSpan div = t5.Divide(2.0);
+
+            Console.WriteLine("t5: " + t5);
+            Console.WriteLine("t6: " + t6);
+            Console.WriteLine("Soma t5 e t6: " + sum);
+            Console.WriteLine("Subtração t5 e t6: " + dif);
+            Console.WriteLine("Multiplicação t5 e t6: " + mult);
+            Console.WriteLine("Divisão t5 e t6: " + div);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Cshap Poo/Secao7TopicosespeciaisEmCSahrp/OperacoesComDateTime/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace OperacoesComDateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d = new DateTime(2001, 8, 15, 13, 45, 58, 275);
+            DateTime d1 = new DateTime(2001, 8, 15, 13, 45, 58);
+            
+
+            Console.WriteLine(d);
+            Console.WriteLine("1) Date: " + d.Date);
+            Console.WriteLine("2) Day: " + d.Day);
+            Console.WriteLine("3) DayOfWeek: " + d.DayOfWeek);
+            Console.WriteLine("4) DayOfYear: " + d.DayOfYear);
+            Console.WriteLine("5) Hour: " + d.Hour);
+            Console.WriteLine("6) Kind: " + d.Kind);
+            Console.WriteLine("7) Millisecond: " + d.Millisecond);
+            Console.WriteLine("8) Minute: " + d.Minute);
+            Console.WriteLine("9) Month: " + d.Month);
+            Console.WriteLine("10) Second: " + d.Second);
+            Console.WriteLine("11) Ticks: " + d.Ticks);
+            Console.WriteLine("12) TimeOfDay: " + d.TimeOfDay);
+            Console.WriteLine("13) Year: " + d.Year);
+            Console.WriteLine();
+
+            string s1 = d1.ToLongDateString();
+            string s2 = d1.ToLongTimeString();
+            string s3 = d1.ToShortDateString();
+            string s4 = d1.ToShortTimeString();
+
+            string s5 = d1.ToString();
+            string s6 = d1.ToString("yyy-MM-dd HH:mm:ss");
+            string s7 = d1.ToString("yyy-MM-dd HH:mm:ss.fff");
+
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+            Console.WriteLine(s4);
+            Console.WriteLine(s5);
+            Console.WriteLine(s6);
+            Console.WriteLine(s7);
+            Console.WriteLine();
+
+            DateTime d2 = new DateTime(2001, 8, 15, 13, 45, 58);
+
+            DateTime d3 = d2.AddHours(2);
+            DateTime d4 = d2.AddMinutes(3);
+
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+            Console.WriteLine(d4);
+            Console.WriteLine();
+
+            DateTime d5 = DateTime.Now;
+            DateTime d6 = d5.AddDays(7);
+
+            Console.WriteLine(d5);
+            Console.WriteLine(d6);
+            Console.WriteLine();
+
+            DateTime d7 = new DateTime(2000, 10, 15);
+            DateTime d8 = new DateTime(2000, 10, 18);
+
+            TimeSpan t = d8.Subtract(d7);
+            Console.WriteLine(t);
+
+        }
+    }
+}
